eba71b53cdbca9fffb1e384b6e10b41fac27e34a
蔡培杰 <hellophper@qq.com>
2018-06-24 18:29:48 +0800

    add comment

--- contract/comment.sol
+++ contract/comment.sol
@@ -7,22 +7,26 @@ pragma solidity ^0.4.16;
 
 contract comment {
 
+    //留言的struct
     struct Comment{
         bytes name;
         bytes message;
         uint time;
     }
 
+    //保存所有的留言
     Comment[] public commentList;
 
     function contructor(){
 
     }
 
+    //保存留言信息
     function set(bytes name,bytes message,uint time) public {
         commentList.push(Comment({name:name,message:message,time:time}));
     }
 
+    //根据index获取单个留言信息
     function get(uint id) public view returns(bytes name,bytes message,uint time){
         Comment item = commentList[id];
         name = item.name;
@@ -30,6 +34,7 @@ contract comment {
         time = item.time;
     }
 
+    //留言数组的长度
     function length() public view returns(uint len){
         len = commentList.length;
     }
